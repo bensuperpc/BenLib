b87141829bbb59e8a8625fcb896a8a4a1b0c1da7
Bensuperpc <bensuperpc@gmail.com>
2020-10-20 20:01:36 +0200

    Fix build failed on Debug and Coverage

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,2 @@
-gcovr -r . --exclude src/lib/utils/sfml/ --exclude src/bench/ --exclude build/CMakeFiles/ --html-details -o coverage/coverage.html
+gcovr -r . --exclude build/CMakeFiles/ --exclude src/ --html-details -o coverage/coverage.html
 #gcovr -r . --exclude src/test/ --exclude src/game.cpp --exclude src/lib/utils/sfml/ --exclude src/bench/ --exclude build/CMakeFiles/
