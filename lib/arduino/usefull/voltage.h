45fee978ade8ce73bdaf7352ec8eab0b97ab2169
Bensuperpc <bensuperpc@gmail.com>
2021-02-23 20:28:45 +0100

    Add arduino code

---
+++ lib/arduino/usefull/voltage.h
@@ -0,0 +1,13 @@
+/*
+** Bensuperpc, 2016-2019
+** Servolent 2016 and Jeu de réflexe 2017
+** BAC project 2016-2017
+** Source: https://www.codeproject.com/Tips/987180/Arduino-Tips-Tricks
+** >voltage.c
+*/
+
+#ifndef ARDUINO_VOLTAGE_H
+#define ARDUINO_VOLTAGE_H
+
+int GetVccMiliVolts();
+#endif
