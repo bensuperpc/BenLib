8068a92e5b9c4c0d76e8e42253d0b37115f4282d
Bensuperpc <bensuperpc@gmail.com>
2021-03-08 15:37:22 +0100

    findStringInv WIP

--- lib/opencl/kernels/findStringInv.cl
+++ lib/opencl/kernels/findStringInv.cl
@@ -18,8 +18,8 @@
 //                                                          //
 //////////////////////////////////////////////////////////////
 
-#define ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-#define alphabetSize 26
+//#define ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//#define alphabetSize 26
 
 /**
  * \brief Generate Alphabetic sequence from size_t value, A=1, Z=27, AA = 28, AB = 29
@@ -27,9 +27,24 @@
  * \param B return array (char*)
  */
 
-kernel void findStringInv( __global ulong* A, __global char* B)
+
+__kernel void findStringInv(__global ulong *length, __global const uchar *data, __global uint *resultCrc32)
 {
-   
+	__private uint crc = 0xFFFFFFFF; // same as previousCrc32 ^ 0xFFFFFFFF
+	while ((*length)-- != 0) {
+		uchar s = (uchar)crc  ^ *data++;
+        uint low = (s ^ (s << 6)) & 0xFF;
+		uint a = (low * ((1 << 23) + (1 << 14) + (1 << 2)));
+		crc = (crc >> 8) ^ (low * ((1 << 24) + (1 << 16) + (1 << 8))) ^ a ^ (a >> 1) ^ (low * ((1 << 20) + (1 << 12))) ^ (low << 19) ^ (low << 17) ^ (low >> 2);
+	}
+
+	*resultCrc32 = ~crc;
+}
+
+
+/*
+kernel void findStringInv(__global ulong* A, __global char* B)
+{
     __private char alpha[26] = {ALPHABET};
     // If *A < 27
     if (*A < 27) {
@@ -49,4 +64,6 @@ kernel void findStringInv( __global ulong* A, __global char* B)
     }
     //const int idx = get_global_id(0);
     //C[idx] = A[idx] + B[idx];
+   
 }
+ */
--- src/exemple/OpenCL/OpenCL_findStringInv.cpp
+++ src/exemple/OpenCL/OpenCL_findStringInv.cpp
@@ -54,11 +54,12 @@ int main(int argc, char **argv)
 
     try {
         //std::unique_ptr<uint[]> A(new uint[N_ELEMENTS]);
-        
+
         auto A = std::make_unique<std::array<unsigned char, N_ELEMENTS>>(std::array<unsigned char, N_ELEMENTS>{'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'});
 
-        std::unique_ptr<uint> B(new uint);
-        B = std::make_unique<uint>(N_ELEMENTS);
+        //std::unique_ptr<uint64_t> B(new uint64_t);
+        //B = std::make_unique<uint64_t>(N_ELEMENTS);
+        uint64_t B[1] = {N_ELEMENTS};
         //std::unique_ptr<int[]> C(new int[N_ELEMENTS]);
         std::unique_ptr<uint> C(new uint);
         C = std::make_unique<uint>(0);
@@ -78,95 +79,14 @@ int main(int argc, char **argv)
         cl::CommandQueue queue = cl::CommandQueue(context, devices[device_id]); // Select the device.
 
         // Create the memory buffers
-        cl::Buffer bufferA = cl::Buffer(context, CL_MEM_WRITE_ONLY, N_ELEMENTS * sizeof(unsigned char));
+        cl::Buffer bufferA = cl::Buffer(context, CL_MEM_READ_ONLY, N_ELEMENTS * sizeof(unsigned char));
+        cl::Buffer bufferB = cl::Buffer(context, CL_MEM_READ_WRITE, sizeof(uint64_t));
         cl::Buffer bufferC = cl::Buffer(context, CL_MEM_WRITE_ONLY, N_ELEMENTS * sizeof(uint));
 
         // Copy the input data to the input buffers using the command queue.
         queue.enqueueWriteBuffer(bufferA, CL_FALSE, 0, N_ELEMENTS * sizeof(unsigned char), A.get());
+        queue.enqueueWriteBuffer(bufferB, CL_FALSE, 0, sizeof(uint64_t), B);
         queue.enqueueWriteBuffer(bufferC, CL_FALSE, 0, N_ELEMENTS * sizeof(uint), C.get());
-
-        // Read the program source
-        std::ifstream sourceFile(KERNEL_FILE);
-        std::string sourceCode(std::istreambuf_iterator<char>(sourceFile), (std::istreambuf_iterator<char>()));
-        cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length()));
-
-        // Make program from the source code
-        cl::Program program = cl::Program(context, source);
-        
-        // Build the program for the devices
-        program.build(devices);
-        
-        // Make kernel
-        cl::Kernel vecadd_kernel(program, FUNCTION_NAME);
-        
-        // Set the kernel arguments
-        vecadd_kernel.setArg(0, bufferA); // Data
-        vecadd_kernel.setArg(1, (uint64_t)N_ELEMENTS); // lenght
-        vecadd_kernel.setArg(2, 0); // Previous CRC
-        vecadd_kernel.setArg(3, bufferC);
-
-        // Execute the kernel
-        cl::NDRange global(N_ELEMENTS);
-        cl::NDRange local(256);
-        
-        queue.enqueueNDRangeKernel(vecadd_kernel, cl::NullRange, global, cl::NullRange);
-        
-        // Copy the output data back to the host
-        std::cout << std::hex << "CRC OpenCL: 0x" << *C << std::endl;
-    }
-    catch (cl::Error err) {
-        std::cout << "Error: " << err.what() << "(" << err.err() << ")" << std::endl;
-        return (EXIT_FAILURE);
-    }
-
-    std::cout << "Done.\n";
-    return (EXIT_SUCCESS);
-}
-
-/*
-int main(int argc, char **argv)
-{
-    // std::cout << crc32Lookup[0][1] << std::endl;
-    const int N_ELEMENTS = 11;
-    unsigned int platform_id = 0, device_id = 0;
-
-    try {
-        //std::unique_ptr<uint[]> A(new uint[N_ELEMENTS]);
-
-        auto A = std::make_unique<std::array<uint, 11>>(std::array<uint, 11>{72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100});
-
-        std::unique_ptr<uint> B(new uint);
-        B = std::make_unique<uint>(5);
-        std::unique_ptr<int[]> D(new int[N_ELEMENTS]);
-
-        std::unique_ptr<uint> C(new uint);
-        C = std::make_unique<uint>(1);
-
-        // Query for platforms
-        std::vector<cl::Platform> platforms;
-        cl::Platform::get(&platforms);
-
-        // Get a list of devices on this platform
-        std::vector<cl::Device> devices;
-        platforms[platform_id].getDevices(CL_DEVICE_TYPE_GPU | CL_DEVICE_TYPE_CPU, &devices); // Select the platform.
-
-        // Create a context
-        cl::Context context(devices);
-
-        // Create a command queue
-        cl::CommandQueue queue = cl::CommandQueue(context, devices[device_id]); // Select the device.
-
-        // Create the memory buffers
-        cl::Buffer bufferA = cl::Buffer(context, CL_MEM_READ_ONLY, N_ELEMENTS * sizeof(uint));
-        //cl::Buffer bufferB = cl::Buffer(context, CL_MEM_READ_ONLY, 1 * sizeof(uint));
-        //cl::Buffer bufferC = cl::Buffer(context, CL_MEM_WRITE_ONLY, 1 * sizeof(uint));
-        cl::Buffer bufferD = cl::Buffer(context, CL_MEM_WRITE_ONLY, N_ELEMENTS * sizeof(uint));
-
-        // Copy the input data to the input buffers using the command queue.
-        queue.enqueueWriteBuffer(bufferA, CL_FALSE, 0, 1 * sizeof(uint), A.get());
-        //queue.enqueueWriteBuffer(bufferB, CL_FALSE, 0, 1 * sizeof(uint), B.get());
-        //queue.enqueueWriteBuffer(bufferC, CL_FALSE, 0, 1 * sizeof(uint), C.get());
-        queue.enqueueWriteBuffer(bufferD, CL_FALSE, 0, N_ELEMENTS * sizeof(uint), D.get());
 
         // Read the program source
         std::ifstream sourceFile(KERNEL_FILE);
@@ -183,11 +103,9 @@ int main(int argc, char **argv)
         cl::Kernel vecadd_kernel(program, FUNCTION_NAME);
         
         // Set the kernel arguments
-        vecadd_kernel.setArg(0, bufferA); // Data
-        vecadd_kernel.setArg(1, N_ELEMENTS); // lenght
-        vecadd_kernel.setArg(2, 1); // 
-        vecadd_kernel.setArg(3, bufferD); // Result
-        
+        vecadd_kernel.setArg(1, bufferA); // Data
+        vecadd_kernel.setArg(0, bufferB); // lenght
+        vecadd_kernel.setArg(2, bufferC);
         // Execute the kernel
         cl::NDRange global(N_ELEMENTS);
         cl::NDRange local(256);
@@ -195,9 +113,8 @@ int main(int argc, char **argv)
         queue.enqueueNDRangeKernel(vecadd_kernel, cl::NullRange, global, cl::NullRange);
         
         // Copy the output data back to the host
-        queue.enqueueReadBuffer(bufferD, CL_TRUE, 0, 1 * sizeof(uint), D.get());
-
-        std::cout << std::hex << D << std::endl;
+        queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, 1 * sizeof(uint), C.get());
+        std::cout << std::hex << "CRC OpenCL: 0x" << *C << std::endl;
     }
     catch (cl::Error err) {
         std::cout << "Error: " << err.what() << "(" << err.err() << ")" << std::endl;
@@ -206,4 +123,4 @@ int main(int argc, char **argv)
 
     std::cout << "Done.\n";
     return (EXIT_SUCCESS);
-}*/
+}
