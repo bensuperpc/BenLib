06c5211d45e2378ca06d2593e634ecc573ea105a
Bensuperpc <bensuperpc@gmail.com>
2021-04-07 08:58:16 +0200

    Improve Toolchain

--- toolchain/toolchain_x86_64_linux_gcc.cmake
+++ toolchain/toolchain_x86_64_linux_gcc.cmake
@@ -12,24 +12,39 @@
 #  Modified: 07, April, 2021                                 #
 #  file: CMakeLists.txt                                      #
 #  CMake                                                     #
-#  Source:                                                   #
+#  Source:      https://stackoverflow.com/questions/15036909/clang-how-to-list-supported-target-architectures                                                   #
 #                                                            #
 #  OS: ALL                                                   #
 #  CPU: ALL                                                  #
 #                                                            #
 ##############################################################
 
+cmake_minimum_required(VERSION 3.10)
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(TOOLCHAIN "x86_64-linux-gnu")
+
+set(CMAKE_ASM_COMPILER gcc)
+set(CMAKE_ASM_COMPILER_TARGET ${TOOLCHAIN})
 
 set(CMAKE_C_COMPILER "gcc")
-set(CMAKE_CXX_COMPILER "g++")
+set(CMAKE_C_COMPILER_TARGET ${TOOLCHAIN})
 
-set(CMAKE_C_FLAGS " ${CMAKE_C_FLAGS} -march=native")
-set(CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS} -march=native")
+set(CMAKE_CXX_COMPILER "g++")
+set(CMAKE_CXX_COMPILER_TARGET ${TOOLCHAIN})
+
+
+set(CMAKE_ASM_FLAGS " ${CMAKE_ASM_FLAGS} -march=skylake")
+set(CMAKE_C_FLAGS " ${CMAKE_C_FLAGS} -march=skylake")
+set(CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS} -march=skylake")
+
+
+#set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES )
+#set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES )
+
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
-
