cf6589e470f85e8e227945ccd2e852150ad98065
Bensuperpc <bensuperpc@gmail.com>
2021-04-08 14:52:31 +0200

    CUDA

--- toolchain/toolchain_x86_64_linux_clang.cmake
+++ toolchain/toolchain_x86_64_linux_clang.cmake
@@ -46,14 +46,40 @@ set(CMAKE_CXX_FLAGS_INIT " ${CMAKE_CXX_FLAGS_INIT} -march=skylake")
 #set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES )
 
 #CUDA
-set(CUDA_TOOLKIT_ROOT_DIR "/usr/local/cuda;/usr;/opt/cuda")
+set(CUDA_TARGET_OS_VARIANT "linux")
+set(CUDA_TARGET_PLATFORM_ARCH ${CMAKE_SYSTEM_PROCESSOR}-${CUDA_TARGET_OS_VARIANT})
 
-#
-#set(CUDA_INCLUDE_DIRS /opt/cuda/include)
-#set(CUDA_CUDART_LIBRARY /opt/cuda/lib64/libcudart.so)
-#set(CUDA_NVCC_EXECUTABLE "/opt/cuda/bin/nvcc")
+set(CMAKE_CUDA_STANDARD 17)
+
+set(CUDA_TOOLKIT_ROOT_DIR "/opt/cuda")
+set(CUDA_TOOLKIT_INCLUDE ${CUDA_TOOLKIT_ROOT_DIR}/include)
+set(CUDA_INCLUDE_DIRS "${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/include")
+
+include_directories(${CUDA_INCLUDE_DIRS} ${CUDA_TOOLKIT_INCLUDE})
+set(CUDA_LIBRARIES "/opt/cuda/lib64/libcudart_static.a;;Threads::Threads;dl;/usr/lib/librt.so")
+
+link_directories("${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib")
+link_directories(${CUDA_LIBRARIES})
+
+#link_directories("${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs")
+
+set(CUDA_CUDART_LIBRARY "${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/libcudart.so")
+set(CUDA_cublas_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libcublas.so)
+set(CUDA_cufft_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libcufft.so)
+set(CUDA_nppc_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppc.so)
+set(CUDA_nppial_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppial.so)
+set(CUDA_nppicc_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppicc.so)
+set(CUDA_nppicom_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppicom.so)
+set(CUDA_nppidei_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppidei.so)
+set(CUDA_nppif_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppif.so)
+set(CUDA_nppig_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppig.so)
+set(CUDA_nppim_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppim.so)
+set(CUDA_nppist_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppist.so)
+set(CUDA_nppisu_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppisu.so)
+set(CUDA_nppitc_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnppitc.so)
+set(CUDA_npps_LIBRARY ${CUDA_TOOLKIT_ROOT_DIR}/targets/${CUDA_TARGET_PLATFORM_ARCH}/lib/stubs/libnpps.so)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
