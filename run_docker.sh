1a1d1879b7306ef296493459f698ebd22f1974f9
Bensuperpc <bensuperpc@gmail.com>
2021-04-29 00:41:15 +0200

    Test docker 13

--- run_docker.sh
+++ run_docker.sh
@@ -14,21 +14,21 @@
 # ==============================================================================
 
 docker run --rm -it --name ben_lib_builder_00 \
---mount type=bind,source="$(pwd)",destination=/usr/src/app,readonly \
+--mount type=bind,source="$(pwd)",destination=/usr/src/app \
 --mount type=tmpfs,destination=/usr/src/app/build,tmpfs-size=128m \
 ben_lib_builder_ubuntu_20.04
 
 docker run --rm -it --name ben_lib_builder_01 \
---mount type=bind,source="$(pwd)",destination=/usr/src/app,readonly \
+--mount type=bind,source="$(pwd)",destination=/usr/src/app \
 --mount type=tmpfs,destination=/usr/src/app/build,tmpfs-size=128m \
 ben_lib_builder_ubuntu_20.10
 
 docker run --rm -it --name ben_lib_builder_02 \
---mount type=bind,source="$(pwd)",destination=/usr/src/app,readonly \
+--mount type=bind,source="$(pwd)",destination=/usr/src/app \
 --mount type=tmpfs,destination=/usr/src/app/build,tmpfs-size=128m \
 ben_lib_builder_ubuntu_21.04
 
 docker run --rm -it --name ben_lib_builder_03 \
---mount type=bind,source="$(pwd)",destination=/usr/src/app,readonly \
+--mount type=bind,source="$(pwd)",destination=/usr/src/app \
 --mount type=tmpfs,destination=/usr/src/app/build,tmpfs-size=128m \
 ben_lib_builder_archlinux
